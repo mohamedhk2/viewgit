8f93dea083c30e6f62fb22dd1fedd61ced1cbf41
Heikki Hokkanen <hoxu@users.sf.net>
2008-06-29 18:02:35 +0300

    Added demo and download page stubs.

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+$title = 'Demo';
+require('inc/header.php');
+?>
+
+<p>To be created...</p>
+
+<?php
+require('inc/footer.php');
+?>
---
+++ download.php
@@ -0,0 +1,11 @@
+<?php
+require('inc/header.php');
+?>
+
+<p>There are no releases yet... :-(</p>
+
+<p>You could also try out the newest <a href="development">development version</a>.</p>
+
+<?php
+require('inc/footer.php');
+?>
