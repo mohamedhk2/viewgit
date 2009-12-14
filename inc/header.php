6987a6bfb5dd49c719c0c51c0be76f3b933b0933
Heikki Hokkanen <hoxu@users.sf.net>
2009-12-14 17:44:46 +0200

    Added meta description & keywords.

--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,8 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 	<title><?php echo isset($title) ? "$title - " : ''; ?>ViewGit - a git repository browser</title>
 	<link rel="stylesheet" href="default.css" type="text/css" />
 	<meta name="author" content="Heikki Hokkanen" />
+	<meta name="description" content="ViewGit is a git web repository viewer that aims to be easy to set up and upgrade, light on dependencies, and comfortable to use." />
+	<meta name="keywords" content="viewgit, git, frontend, repository, browser, viewer, web" />
 </head>
 <body>
 
