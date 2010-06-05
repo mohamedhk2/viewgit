7c972f85479a3f1723dd181ebfa87a3398e55691
Heikki Hokkanen <hoxu@users.sf.net>
2010-06-05 10:11:32 +0300

    Point git link to http://git-scm.com/

--- requirements.php
+++ requirements.php
@@ -8,7 +8,7 @@ require('inc/header.php');
 <ul>
 	<li><a href="http://www.php.net/">PHP 5</a></li>
 	<li><a href="http://httpd.apache.org/">Apache</a> or a compatible web server</li>
-	<li>Reasonably new <a href="http://git.or.cz/">git</a></li>
+	<li>Reasonably new <a href="http://git-scm.com/">git</a></li>
 </ul>
 
 <?php
