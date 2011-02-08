d34e0715672657ca53dcd1d7927dc043ed49ff15
Heikki Hokkanen <hoxu@users.sf.net>
2011-02-08 18:13:36 +0200

    AGPLv3 or later, not older

--- license.php
+++ license.php
@@ -8,7 +8,7 @@ require('inc/header.php');
 <p>
 ViewGit is licensed under <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html">GNU Affero General Public License version 3</a> (AGPLv3). This license applies for both ViewGit and this website. Also see doc/LICENSE.</p>
 
-<p>Contributions should be sent under "AGPLv3 or older" to allow me to upgrade the license later on.</p>
+<p>Contributions should be sent under "AGPLv3 or later" to allow me to upgrade the license later on.</p>
 
 <pre class="license">
 <?php
